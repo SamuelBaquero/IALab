0ca1cd3d0551559b5d1d0ac38944eb4c9d7676e7
SamuelBaquero <s.baquero10@uniandes.edu.co>
2017-09-15 23:35:16 -0500

    Commit inicial del proyecto, creada estructura de paquetes del front (ReactAppGenerated) y del back (Express Generated APP).

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hola Mundo
+      </div>
+    );
+  }
+}
+
+export default App;
